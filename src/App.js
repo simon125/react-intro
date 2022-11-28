cd140a5c738906d2f8a5c8ee2bc25db7759231cc
Szymon Oleszek <szymqwerty@gmail.com>
2022-11-28 21:33:58 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [todoName, setTodoName] = useState("");
+  const [status, setStatus] = useState(false);
+
+  const handleIncrementClick = () => {
+    setCounter(counter + 1);
+  };
+
+  const handleDecrementClick = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleChange = (event) => {
+    setTodoName(event.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{counter}</h1>
+      <p>{todoName}</p>
+      <p>{status ? "Done" : "In progress"}</p>
+      <input
+        value={todoName}
+        onChange={handleChange}
+        type="text"
+        placeholder="Enter todo name..."
+      />
+      <input
+        type="checkbox"
+        checked={status}
+        onChange={() => setStatus(!status)}
+      />
+      <button onClick={handleIncrementClick}>+1</button>
+      <button onClick={handleDecrementClick}>-1</button>
     </div>
   );
 }
 
 export default App;
+
+// class App1 extends React.Component {
+//   state = {
+//     todoName: "",
+//     todo: [],
+//   };
+
+//   handleChange = (event) => {
+//     this.setState({});
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <input type="text" onChange={this.handleChange} />
+//       </div>
+//     );
+//   }
+// }
